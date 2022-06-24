b328624c6bf25d650d555c4c428bf74a1d746a77
DannnLim <Dannylim-@hotmail.com>
2022-06-24 21:42:15 +1000

    commit tree 5ef70323a3e4bb1e1caac7a9db97c8c25ac7b5da

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution here!
+let append = ["Milo", "Otis", "Garfield"]
+function members(append) {
+    append.push('Odie')
+    console.log(append)
+}
+
+console.log(members(append))
+
+let prepend = ["Milo", "Otis", "Garfield"]
+function member(prepend) {
+    prepend.unshift('Odie')
+    console.log(prepend)
+}
+console.log(member(prepend))
+
+let removeLast = ["Milo", "Otis", "Garfield"]
+function memberRl(removeLast) {
+    removeLast = removeLast.pop()
+    console.log(removeLast.pop());
+
+}
+console.log(removeLast.pop())
+console.log(removeLast)
+
+let removeFirst = ["Milo", "Otis", "Garfield"]
+function memberRf(removeFirst) {
+    console.log(removeFirst.splice(0, 1))
+}
+console.log(removeFirst.splice(0, 1))
